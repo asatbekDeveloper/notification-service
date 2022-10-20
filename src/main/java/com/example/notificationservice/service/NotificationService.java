3c7638b3932ae822b9a04f2a404c45deb320f67a
Akmal MIngyasharov <mr.akmal4555@mail.ru>
2022-10-20 15:48:44 +0500

    initial

--- src/main/java/com/example/notificationservice/service/NotificationService.java
+++ src/main/java/com/example/notificationservice/service/NotificationService.java
@@ -4,18 +4,29 @@ package com.example.notificationservice.service;
 import com.example.notificationservice.dto.RabbitDTO;
 import lombok.SneakyThrows;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public NotificationService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @RabbitListener(queues = "${rabbitmq.queue}")
     public void receive(RabbitDTO dto) {
         sendNotification(dto);
     }
 
+
     @SneakyThrows
     public void sendNotification(RabbitDTO dto) {
+
+        messagingTemplate.convertAndSend("/topic/notification",dto);
 
     }
 }
